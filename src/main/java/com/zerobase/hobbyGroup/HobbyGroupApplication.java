dab3055bd200e4abb62aef9af734e64ef573852a
junghoon <hoon2207@naver.com>
2024-03-27 14:49:43 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/hobbyGroup/HobbyGroupApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.hobbyGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/hobbyGroup/HobbyGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.hobbyGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
